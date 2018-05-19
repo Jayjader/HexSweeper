6b52a9ef92951fbe903e1a201f2d33573952cb46
Jayjader <njay47@gmail.com>
2018-05-19 17:12:41 +0200

    Fix bounding on computation of which hex was clicked

---
+++ map/MapOverlay.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var hexes = []
+const fira_font = preload("res://assets/fira-code-dynamic-font.tres")
+
+func _ready():
+	fira_font.size = 10
+	fira_font.update_changes()
+
+func _draw():
+	for hex in self.hexes:
+		var n = String(hex.neighbors)
+		var offset = Transform2D().scaled(0.5 * Vector2(-1, 1)) * fira_font.get_string_size(n)
+		offset = offset.snapped(Vector2(1, -1))
+		draw_string(fira_font, hex.position + offset, n)
+	self.hexes = []

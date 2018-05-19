f439c6e9bc80f9b93878c0889be1b9d5737e0350
Jayjader <njay47@gmail.com>
2018-05-19 18:28:58 +0200

    Propagate reveal of empty hexes
    Add `x`- and `y_bounds` to `Map`
    Wrap revealing individual hexes with a utility function that propagates empty hex reveals to neighbors
    Set `MapHex.offset_position` & `MapHex.neighbors` during map generation

--- map/Map.gd
+++ map/Map.gd
@@ -8,6 +8,9 @@ export (int) var width
 export (int) var height
 export (int) var hexScale
 
+onready var x_bounds = [0, width]
+onready var y_bounds = [0, height]
+
 var _map = {}
 
 var hexHeight = 2
@@ -18,6 +21,24 @@ var yDistBetweenHexes = .75 * hexHeight
 func xOffset(rowNumber):
 	return -.5 * hexWidth * (rowNumber % 2)
 
+func is_on_map(offset):
+	return offset.x >= self.x_bounds[0] && offset.x < self.x_bounds[1] && offset.y >= self.y_bounds[0] && offset.y < self.y_bounds[1]
+
+func reveal(offsets):
+	match offsets:
+		[]:
+			pass
+		[var offset, ..]:
+			var hex = self._map[offset]
+			if hex.visual_state == hex.STATE.UNDISCOVERED:
+				hex.reveal()
+				if hex.neighbors == 0:
+					for n in Util.offset_neighbors(hex.offset_position):
+						if self.is_on_map(n):
+							offsets.append(n)
+			offsets.pop_front()
+			self.reveal(offsets)
+
 func _ready():
 	fira_font.size = 10
 	fira_font.update_changes()
@@ -25,6 +46,7 @@ func _ready():
 	for y in range(self.height):
 		for x in range(self.width):
 			var hex = preload("res://map/MapHex.tscn").instance()
+			hex.offset_position = Vector2(x, y)
 			hex.transform = hex.transform.translated(Vector2(
 				self.hexWidth * x + self.xOffset(y), self.yDistBetweenHexes * y))
 			hex.transform = hex.transform.scaled(hexScale * Vector2(1, 1))
@@ -32,20 +54,23 @@ func _ready():
 			self._map[Vector2(x, y)] = hex
 			self.add_child(hex, true)
 
+	for hex in self._map.values():
+		for neighbor in Util.offset_neighbors(hex.offset_position):
+			if self.is_on_map(neighbor) && self._map[neighbor].mine:
+				hex.neighbors += 1
+
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT && event.is_pressed():
 			var click_position = self.get_local_mouse_position()
 			var hex = Util.cube_to_evenr(Util.pixel_to_cube(click_position, self.hexScale))
-			if 0 <= hex.x && hex.x < width:
-				if 0 <= hex.y && hex.y < height:
-					self._map[hex].reveal()
-					update()
+			if self.is_on_map(hex):
+				self.reveal([hex])
+				update()
 
 func _draw():
 	for hex in self._map.values():
 		if hex.visual_state == hex.STATE.EMPTY:
 			$"MapOverlay".hexes.append(hex)
-			# draw_string(fira_font, hex.position, "TEST STRING")
 	$"MapOverlay".update()
--- map/MapOverlay.gd
+++ map/MapOverlay.gd
@@ -4,10 +4,15 @@ var hexes = []
 const fira_font = preload("res://assets/fira-code-dynamic-font.tres")
 
 func _ready():
-    fira_font.size = 10
-    fira_font.update_changes()
+	fira_font.size = 10
+	fira_font.update_changes()
 
 func _draw():
-    for hex in self.hexes:
-        draw_string(fira_font, hex.position, String(hex.neighbors))
-    self.hexes = []
+	for hex in self.hexes:
+		var n = hex.neighbors
+		if n > 0:
+			n = String(n)
+			var offset = Transform2D().scaled(0.5 * Vector2(-1, 1)) * fira_font.get_string_size(n)
+			offset = offset.snapped(Vector2(1, -1))
+			draw_string(fira_font, hex.position + offset, n)
+	self.hexes = []
